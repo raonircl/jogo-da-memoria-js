d0a0fd2aa1ec5ec71d26f7224947a9755d83de3b
raonircl <raoni.afi@gmail.com>
2024-02-06 07:20:13 -0300

    feat: função checkMatch e estilos da boxOpen e boxMatch

--- src/js/engine.js
+++ src/js/engine.js
@@ -14,7 +14,7 @@ const emojis = [
     "🧚‍♂️",
     "🧚‍♂️",
     "🎃",
-    "🎃",
+    "🎃"
 ];
 
 let openCards = [];
@@ -25,21 +25,33 @@ for (let i = 0; i < emojis.length; i++) {
     let box =  document.createElement("div");
     box.className = "item";
     box.innerHTML = shuffleEmojis[i];
-    box.onlick = handleClick();
+    box.onclick = handleClick;
     document.querySelector('.game').appendChild(box);
 }
 
-const handleClick = () => {
+function handleClick () {
     if(openCards.length < 2) {
         this.classList.add("boxOpen");
         openCards.push(this);
     }
-
+    
     if(openCards.length == 2) {
         setTimeout(checkMatch, 500);
     }
 };
 
-const checkMatch = () => {
-    
-};
+function checkMatch () {
+    if (openCards[0].innerHTML === openCards[1].innerHTML) {
+        openCards[0].classList.add("boxMatch");
+        openCards[1].classList.add("boxMatch");
+    } else {
+        openCards[0].classList.remove("boxOpen");
+        openCards[1].classList.remove("boxOpen");
+    }
+
+    openCards = [];
+
+    if (document.querySelectorAll(".boxMatch").length === emojis.length) {
+        alert("Você venceu!");
+    }
+}
