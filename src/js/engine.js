ae669975843e314715b177ea83476f878595b721
raonircl <raoni.afi@gmail.com>
2024-02-05 21:33:15 -0300

    feat: array de emojis

---
+++ src/js/engine.js
@@ -0,0 +1,29 @@
+const emojis = [
+    "🐱‍🐉",
+    "🐱‍🐉",
+    "🐱‍👓",
+    "🐱‍👓",
+    "🐱‍🚀",
+    "🐱‍🚀",
+    "🧛‍♀️",
+    "🧛‍♀️",
+    "🧙‍♂️",
+    "🧙‍♂️",
+    "🧟‍♀️",
+    "🧟‍♀️",
+    "🧚‍♂️",
+    "🧚‍♂️",
+    "🎃",
+    "🎃",
+];
+
+let openCards = [];
+
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+
+for (let i = 0; i < emojis.length; i++) {
+    let box =  document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    document.querySelector('.game').appendChild(box);
+}
